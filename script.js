d88007c71cabf321838acf47d38baa943705923b
Megha Subramanya <56509196+meghasubramanya@users.noreply.github.com>
2020-06-26 19:56:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-var names=["John","Alen","Megha","jenny","Eric","jack","james","Laura"];
-for(var i=0;i<names.length;i++)
-{
-	if(names[i].charAt(0)=='j'||names[i].charAt(0)=='J')
-	{
-		console.log("Goodbye "+names[i]);
-	}
-	else
-	{
-		console.log("Hello "+names[i]);
-	}
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  
+for(var i = 0; i < names.length; i++){
+  if(names[i].charAt(0) == 'j' || names[i].charAt(0) == 'J'){
+    console.log("Good Bye " + names[i]);
+  }
+  else{
+    console.log("Hello " + names[i]);
+  }
 }
